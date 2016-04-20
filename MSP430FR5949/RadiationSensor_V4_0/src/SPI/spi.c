46b1e71f5ef377220177956d989625d851f354db
MCasari-PMEL <matthew.casari@noaa.gov>
2016-04-19 17:18:36 -0700

    Added SPI files

---
+++ MSP430FR5949/RadiationSensor_V4_0/src/SPI/spi.c
@@ -0,0 +1,18 @@
+
+/** @file spi.h
+*  @brief
+*
+*  @author Matt Casari, matthew.casari@noaa.org
+*  @date 
+*  @version 
+*
+*  @copyright National Oceanic and Atmospheric Administration
+*  @copyright Pacific Marine Environmental Lab
+*  @copyright Environmental Development Division
+*
+*  @note
+*
+*  @bug  No known bugs
+*/
+#include "spi.h"
+
